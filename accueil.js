4d4c311baa4c53ac69beb8e06c27cbfb74a2bb2a
Mickazh <127355080+Mickazh@users.noreply.github.com>
2023-10-03 21:11:25 +0200

    ajout gitignore pour test

--- accueil.js
+++ accueil.js
@@ -1,8 +1,10 @@
+import {WEATHER_API_KEY} from "./config.js";
+
 const navBar=document.getElementById("NavBar");
 const divMeteo=document.getElementById("meteo");
 const divTemperature=document.getElementById("temperature");
 const meteoIcon=document.getElementById("meteoIcon")
-const WEATHER_API_KEY="7d4fed23c3b77156f7cc9ac02a44fc32";
+// const WEATHER_API_KEY="7d4fed23c3b77156f7cc9ac02a44fc32";
 const WEATHER_API_URL="https://api.openweathermap.org/data/2.5/weather?&units=metric&lang=french"
 const map_weather={"Snow":"img/meteo/neige.png",
                 "Clear":"img/meteo/soleil.png",
---
+++ scripts/transport.js
@@ -0,0 +1,3 @@
+import {WEATHER_API_KEY} from "../config.js";
+
+console.log(WEATHER_API_KEY);

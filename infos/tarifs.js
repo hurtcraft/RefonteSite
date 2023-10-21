893ced44511ade79c2d8ef66f6e55df8a9e1c415
Mickazh <127355080+Mickazh@users.noreply.github.com>
2023-10-22 00:38:45 +0200

    ajout des tarifs pas encore fini + bug avec qualites

---
+++ infos/tarifs.js
@@ -0,0 +1,92 @@
+export const jeuneTarif = `Les nouveautés de cet abonnement :
+
+ 
+
+• L'abonnement est valable pour une année du 1er septembre au 31 août.
+
+• L'inscription est à renouveler tous les ans sur www.corolis.fr
+
+• Valable toute l'année sur toutes les lignes scolaires, interurbaines et urbaines du réseau Corolis excepté la Navette Express Hôtels, la ligne 518 et le service Corolis à la Demande.
+
+ 
+
+Les modalités de souscription :
+
+ 
+
+Pour obtenir l'abonnement à la rentrée, tous les enfants, même beauvaisiens, doivent désormais passer par le portail d'inscription.
+
+ 
+
+Pour s'inscrire, cliquez-ici. 
+
+ 
+
+Pour être certain d'être en règle et de voyager en toute tranquillité dès la rentrée de septembre, il est conseillé de s'inscrire avant le 11 août 2023.
+
+ 
+
+Pour télécharger le dépliant : ici 
+
+ 
+
+Pour être accompagné pas à pas, consultez nos tutoriels en cliquant- ici
+
+ 
+
+Vous trouverez ci-après les réponses aux questions les plus fréquemment posées : ici
+
+ 
+
+VOTRE ENFANT UTILISE ÉGALEMENT UNE LIGNE DE LA RÉGION HAUTS-DE-FRANCE POUR SES VOYAGES ENTRE SON DOMICILE ET SON ÉTABLISSEMENT SCOLAIRE ?
+
+
+Pour le savoir, consultez le document "Où m'inscrire pour mes transports scolaires ?" en cliquant ici`
+
+export const retraiteTarif = `À partir du 1er septembre 2023, la gratuité est obtenue en attestant de votre qualité de retraité. La notification de retraite est délivrée par les caisses de retraite du régime général.
+
+ 
+
+> Souscription en agence Corolis
+
+ 
+
+Pour télécharger le dépliant : ici 
+
+ 
+
+(Titres non valables sur les lignes RPI, la ligne 518, la Navette Express Hôtels et le service Corolis à la Demande).`
+
+export const emploiTarif = `Les modalités restes inchangés. Vous devez présenter chaque mois votre attestation Pôle emploi pour profiter de la gratuité. 
+
+ 
+
+> Souscription en agence Corolis
+
+ 
+
+(Titre non valable sur les lignes à vocation scolaire, la Navette Express Hôtels et le service Corolis à la Demande).`
+
+export const invalideTarif = `Les modalités restent inchangées. Vous devez présenter chaque année votre Carte Mobilité Inclusion mention Invalidité pour profiter de la gratuité.
+
+ 
+
+> Souscription en agence Corolis
+
+ 
+
+(Titre non valable sur les lignes à vocation scolaire, la Navette Express Hôtels et le service Corolis à la Demande).`
+
+export const publicTarif = `À partir du 1er septembre 2023, la réduction est obtenue grâce au quotient familial.
+
+Le quotient familial est un indicateur du niveau de vie de votre foyer fiscal.
+
+Il est calculé par les services de la CAF ou MSA si vous êtes allocataire ou à partir de votre dernier avis d’imposition, si vous n’êtes pas allocataire.
+
+ 
+
+> Souscription en agence Corolis
+
+ 
+
+Pour télécharger le dépliant : ici `
